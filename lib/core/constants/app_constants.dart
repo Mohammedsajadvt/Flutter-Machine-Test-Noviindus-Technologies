e7708a4a329f3e74fcf7f4565a595f3c2cc4d2d2
Mohammed Sajad VT <mohammedsajadvt@gmail.com>
2025-09-16 17:29:54 +0530

    Added urls

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,8 @@
+class AppConstants {
+  static const String baseUrl = "https://flutter-amr.noviindus.in/api";
+
+  // Endpoints
+  static const String loginEndpoint = "/Login";
+  static const String homeEndpoint = "/HomeApi";
+  static const String branchListEndpoint = "/BranchList";
+}
